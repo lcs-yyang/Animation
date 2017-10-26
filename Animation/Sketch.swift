203211cfec4263a472b8df3221830d3e92905b69
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2017-10-26 11:17:36 -0400

    challenge 1

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,10 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    //change in position of circle
+    var dx : Int
+    
+    
     // This function runs once
     override init() {
         
@@ -18,17 +22,36 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
-    }
+        // Set the change value
+        dx = 3
+        
+        }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        //clean up, draw a white rectangle over the whole canvas.
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
         
+        // Change position
+        x += dx
+        
+        // Make the circle bounce on the right edge
+        if x > 500 { // start of the block
+            dx = -3 // move left
+        } // end of block
+        
+        // Make the circle bounce on the left edge
+        if x < 0 { // start of the block
+            dx = 3 // move right
+        } //end of block
+
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
+
     
 }
