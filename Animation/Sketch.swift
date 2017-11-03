f8e0b028507bc8eff4c7e57f62761c8dfecb221f
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2017-11-03 10:55:59 -0400

    Finishing the bouncing ball

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,9 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var dx: Int
+    var y : Int
+    var dy: Int
     // This function runs once
     override init() {
         
@@ -16,19 +18,41 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
-        
+        x = random(from: 0, toButNotIncluding: 501)
+        dx = 5
+        dy = 5
+        y = random(from: 0, toButNotIncluding: 501)
+        // No border
+        canvas.drawShapesWithBorders = false
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        //CLean up from the last time
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
         
+        // Change position
+        x += dx
+        y += dy
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        if x > 500 { //move left when hit the right edge
+            dx = -5
+        if y > 500 { //move left when hit the right edge
+            dy = -5}
+        }
+        if x < 0 { // move right when hit the left edge
+            dx = 5
+        if y < 0 { // move right when hit the left edge
+            dy = 5}
+        }
         
     }
     
 }
+
+
+    
